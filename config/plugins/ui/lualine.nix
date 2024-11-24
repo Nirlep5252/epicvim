419a7f789095639fcd8a3db6a2792895e2abd5c3
nirlep5252 <hello@nirlep.dev>
2024-11-24 20:05:37 +0530

    slightly clearer lualine

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -3,73 +3,45 @@
     enable = true;
     settings = {
       globalstatus = true;
-      disabled_filetypes = {
-        statusline = [
-          "dashboard"
-          "alpha"
-          "starter"
-        ];
-      };
+      disabled_filetypes = { statusline = [ "dashboard" "alpha" "starter" ]; };
       sections = {
-        lualine_a = [
-          {
-            __unkeyed = "mode";
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
-        lualine_b = [
-          {
-            __unkeyed = "branch";
-            icon = "";
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
-        lualine_c = [
-          {
-            __unkeyed = "diagnostic";
-            symbols = {
-              error = " ";
-              warn = " ";
-              info = " ";
-              hint = "󰝶 ";
-            };
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
-        lualine_x = [
-          {
-            __unkeyed = "encoding";
-            separator.left = "";
-            separator.right = "";
-          }
-          {
-            __unkeyed = "fileformat";
-            separator.left = "";
-            separator.right = "";
-          }
-          {
-            __unkeyed = "filetype";
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
-        lualine_y = [
-          {
-            __unkeyed = "progress";
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
-        lualine_z = [
-          {
-            __unkeyed = "location";
-            separator.left = "";
-            separator.right = "";
-          }
-        ];
+        lualine_a = [{
+          __unkeyed = "mode";
+          separator.left = "";
+          separator.right = "";
+        }];
+        lualine_b = [{
+          __unkeyed = "branch";
+          icon = "";
+          separator.left = "";
+          separator.right = "";
+        }];
+        lualine_c = [{
+          __unkeyed = "diagnostic";
+          symbols = {
+            error = " ";
+            warn = " ";
+            info = " ";
+            hint = "󰝶 ";
+          };
+          separator.left = "";
+          separator.right = "";
+        }];
+        lualine_x = [{
+          __unkeyed = "filename";
+          separator.left = "";
+          separator.right = "";
+        }];
+        lualine_y = [{
+          __unkeyed = "progress";
+          separator.left = "";
+          separator.right = "";
+        }];
+        lualine_z = [{
+          __unkeyed = "location";
+          separator.left = "";
+          separator.right = "";
+        }];
       };
     };
   };
